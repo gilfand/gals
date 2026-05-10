d30b732140ea2feb446255aec3c7f653f88f8356
Alexander Gilfand <gilfand@googlemail.com>
2026-05-11 00:46:10 +0200

    port 80

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ if __name__ in {"__main__", "__mp_main__"}:
         IndustrialApp()
         ui.run(
             host="127.0.0.1",
-            port=8080,
+            port=80,
             reload=True,
             dark=True,
             title="Промышленная Платформа"

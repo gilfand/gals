0a5fa8bb8fdafaa97f934a9209a6971d4724e131
Alexander Gilfand <gilfand@googlemail.com>
2026-05-13 14:12:30 +0200

    Fix Dockerfile with stable nicegui:2.0.0

--- main.py
+++ main.py
@@ -151,9 +151,9 @@ if __name__ in {"__main__", "__mp_main__"}:
     try:
         IndustrialApp()
         ui.run(
-            host="127.0.0.1",
+            host="0.0.0.0",
             port=80,
-            reload=True,
+            reload=False,
             dark=True,
             title="Промышленная Платформа"
         )
